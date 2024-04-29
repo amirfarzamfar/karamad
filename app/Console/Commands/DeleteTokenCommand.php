5b8d5b22efcd955380f228c09ca1101900def74e
amirreza <amirrezafarzamfar@gmail.com>
2024-04-29 14:46:42 +0330

    cron

---
+++ app/Console/Commands/DeleteTokenCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DeleteTokenCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-token-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'delete token command';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ app/Console/Commands/DeleteUnverifiedUserCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class DeleteUnverifiedUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-unverified-user-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove unverified users after 4 minutes';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        User::where('phone_verified_at', null)
+            ->where('created_at', '<', now()->subMinutes(4))
+            ->delete();
+    }
+}
