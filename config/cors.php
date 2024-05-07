80fdcc9256d2a187656833da9e44c1671aa2acbc
XveleronX <bidkhoryt@gmail.com>
2024-05-07 14:12:37 +0330

    fixing cors 3

--- config/cors.php
+++ config/cors.php
@@ -17,13 +17,13 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*','http://localhost:3000'],
 
     'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*' , 'Content-Type', 'X-Requested-With'],
 
     'exposed_headers' => [],
 
