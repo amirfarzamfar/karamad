565658d52637a6b92aeb69dcaf7af1aac1785f24
XveleronX <bidkhoryt@gmail.com>
2024-05-07 12:32:59 +0330

    fixing cors 2

--- config/cors.php
+++ config/cors.php
@@ -19,11 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['content-type' , 'X-Requested-With'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
